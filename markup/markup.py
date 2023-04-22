d3e7c8ecb3843e491a5e232d92e8a43c0ec95204
Nickita <118798761+NickitaSh@users.noreply.github.com>
2023-04-22 10:55:52 +0500

    Add button categories product

--- markup/markup.py
+++ markup/markup.py
@@ -1,5 +1,6 @@
 # импортируем специальные типы телеграм бота для создания элементов интерфейса
-from telebot.types import KeyboardButton, ReplyKeyboardMarkup
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup, \
+    ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 # импортируем настройки и утилиты
 from settings import config
 # импортируем класс-менеджер для работы с библиотекой
@@ -32,7 +33,7 @@ class Keyboards:
         itm_btn_1 = self.set_btn('CHOOSE_GOODS')
         itm_btn_2 = self.set_btn('INFO')
         itm_btn_3 = self.set_btn('SETTINGS')
-        # расположение кнопок в меню
+        # рассположение кнопок в меню
         self.markup.row(itm_btn_1)
         self.markup.row(itm_btn_2, itm_btn_3)
         return self.markup
@@ -43,7 +44,7 @@ class Keyboards:
         """
         self.markup = ReplyKeyboardMarkup(True, True)
         itm_btn_1 = self.set_btn('<<')
-        # расположение кнопок в меню
+        # рассположение кнопок в меню
         self.markup.row(itm_btn_1)
         return self.markup
 
@@ -53,6 +54,45 @@ class Keyboards:
         """
         self.markup = ReplyKeyboardMarkup(True, True)
         itm_btn_1 = self.set_btn('<<')
-        # расположение кнопок в меню
+        # рассположение кнопок в меню
         self.markup.row(itm_btn_1)
+        return self.markup
+
+    @staticmethod
+    def remove_menu():
+        """
+        Удаляет меню
+        """
+        return ReplyKeyboardRemove()
+
+    def category_menu(self):
+        """
+        Создает разметку кнопок в меню категорий товара и возвращает разметку
+        """
+        self.markup = ReplyKeyboardMarkup(True, True, row_width=1)
+        self.markup.add(self.set_btn('SEMIPRODUCT'))
+        self.markup.add(self.set_btn('GROCERY'))
+        self.markup.add(self.set_btn('ICE_CREAM'))
+        self.markup.row(self.set_btn('<<'), self.set_btn('ORDER'))
+        return self.markup
+
+    @staticmethod
+    def set_inline_btn(name):
+        """
+        Создает и возвращает инлайн-кнопку по входным параметрам
+        """
+        return InlineKeyboardButton(str(name),
+                                    callback_data=str(name.id))
+
+    def set_select_category(self, category):
+        """
+        Создает разметку инлайн-кнопок в выбранной
+        категории товара и возвращает разметку
+        """
+        self.markup = InlineKeyboardMarkup(row_width=1)
+        # загружаем в названия инлайн-кнопок данные
+        # из БД в соответствие с категорией товара
+        for itm in self.BD.select_all_products_category(category):
+            self.markup.add(self.set_inline_btn(itm))
+
         return self.markup
